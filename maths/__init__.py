6391b1f8dfe333735d334f5ef5cc360c79b0d067
boussooo <mametoutybousso2104@gmail.com>
2026-04-08 16:24:03 +0000

    modified maths/init utils/init

---
+++ maths/__init__.py
@@ -0,0 +1,11 @@
+from .operations import addition, soustraction, multiplication
+from .statistiques import moyenne, maximum, minimum, somme_des_carres
+
+__all__ = [
+    "addition",
+    "soustraction",
+    "multiplication",
+    "moyenne",
+    "maximum",
+    "minimum"
+]
---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .string_utils import inverser_chaine, compter_mots
+
+__all__ = ["inverser_chaine", "compter_mots"]
