7ae2f9ac2e355a011e89c2dcb85f192e136c7f2a
boussooo <mametoutybousso2104@gmail.com>
2026-04-08 16:55:15 +0000

    modified 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from maths.operations import addition, soustraction, multiplication
 from maths.statistiques import moyenne, maximum, minimum
 from utils.string_utils import inverser_chaine, compter_mots
+import requests
 
 # Maths
 print("Addition:", addition(5, 3))
@@ -17,3 +18,18 @@ print("Min:", minimum(liste))
 texte = "Bonjour tout le monde"
 print("Inverse:", inverser_chaine(texte))
 print("Nombre de mots:", compter_mots(texte))
+
+
+
+def tester_requete(url):
+    try:
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            print("Requête réussie !")
+            print(response.text[:300])
+        else:
+            print("Erreur HTTP :", response.status_code)
+    except Exception as e:
+        print("Erreur :", e)
+
+tester_requete("https://example.com")
